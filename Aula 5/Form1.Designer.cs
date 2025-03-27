a82a14d1ccfa9eb2bee7dffd54036c201485e8a4
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2025-03-26 21:54:59 -0300

    Jogo de cartas

--- Aula 5/Form1.Designer.cs
+++ Aula 5/Form1.Designer.cs
@@ -28,18 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pic_baralho = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pic_baralho).BeginInit();
             SuspendLayout();
+            // 
+            // pic_baralho
+            // 
+            pic_baralho.Image = Properties.Resources.capa;
+            pic_baralho.Location = new Point(146, 12);
+            pic_baralho.Name = "pic_baralho";
+            pic_baralho.Size = new Size(230, 352);
+            pic_baralho.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_baralho.TabIndex = 0;
+            pic_baralho.TabStop = false;
+            pic_baralho.Click += pic_baralho_Click;
             // 
             // frm_baralho
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(575, 368);
+            ClientSize = new Size(519, 426);
+            Controls.Add(pic_baralho);
             Name = "frm_baralho";
             Text = "Baralho";
+            ((System.ComponentModel.ISupportInitialize)pic_baralho).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pic_baralho;
     }
 }
--- Aula 5/Form1.cs
+++ Aula 5/Form1.cs
@@ -2,9 +2,35 @@ namespace Aula_5
 {
     public partial class frm_baralho : Form
     {
+        bool troca = true;
         public frm_baralho()
         {
             InitializeComponent();
+        }
+
+        private void pic_baralho_Click(object sender, EventArgs e)
+        {
+
+            Random sorteio = new Random();
+            int numeroCarta = sorteio.Next(1, 14);  // numeros de 1 a 13
+            string naipe = "espada_";
+
+            string carta = naipe + numeroCarta.ToString();
+            // alternativa
+            string novacarta = $"{naipe}{numeroCarta}";
+
+            if ( troca )
+            {
+                //pic_baralho.Image = Properties.Resources.espada_2;
+                pic_baralho.Image = (System.Drawing.Bitmap)Properties.Resources.ResourceManager.GetObject(carta);
+
+                troca = false;
+            }
+            else
+            {
+                pic_baralho.Image = Properties.Resources.capa;
+                troca = true;
+            }
         }
     }
 }

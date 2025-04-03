fedc6cac043e6acc8025ff1d461215d792f43639
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2025-04-02 21:46:53 -0300

    Baralho duplo

--- Aula 5/Form1.Designer.cs
+++ Aula 5/Form1.Designer.cs
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             pic_baralho = new PictureBox();
+            pic_cartas = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pic_baralho).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic_cartas).BeginInit();
             SuspendLayout();
             // 
             // pic_baralho
             // 
             pic_baralho.Image = Properties.Resources.capa;
-            pic_baralho.Location = new Point(146, 12);
+            pic_baralho.Location = new Point(21, 12);
             pic_baralho.Name = "pic_baralho";
             pic_baralho.Size = new Size(230, 352);
             pic_baralho.SizeMode = PictureBoxSizeMode.Zoom;
@@ -43,20 +45,32 @@
             pic_baralho.TabStop = false;
             pic_baralho.Click += pic_baralho_Click;
             // 
+            // pic_cartas
+            // 
+            pic_cartas.Location = new Point(289, 12);
+            pic_cartas.Name = "pic_cartas";
+            pic_cartas.Size = new Size(230, 352);
+            pic_cartas.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_cartas.TabIndex = 1;
+            pic_cartas.TabStop = false;
+            // 
             // frm_baralho
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(519, 426);
+            ClientSize = new Size(541, 426);
+            Controls.Add(pic_cartas);
             Controls.Add(pic_baralho);
             Name = "frm_baralho";
             Text = "Baralho";
             ((System.ComponentModel.ISupportInitialize)pic_baralho).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic_cartas).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox pic_baralho;
+        private PictureBox pic_cartas;
     }
 }
--- Aula 5/Form1.cs
+++ Aula 5/Form1.cs
@@ -3,6 +3,10 @@ namespace Aula_5
     public partial class frm_baralho : Form
     {
         bool troca = true;
+        int[] cartas = new int[13];
+        int indiceCarta = 0;
+        List<int> numerosSorteados = new List<int>();
+
         public frm_baralho()
         {
             InitializeComponent();
@@ -12,15 +16,26 @@ namespace Aula_5
         {
 
             Random sorteio = new Random();
-            int numeroCarta = sorteio.Next(1, 14);  // numeros de 1 a 13
+            int numeroCarta;
             string naipe = "espada_";
+            string carta = "1";
 
-            string carta = naipe + numeroCarta.ToString();
-            // alternativa
-            string novacarta = $"{naipe}{numeroCarta}";
-
-            if ( troca )
-            {
+            if (troca) { 
+                // Gera um número único que ainda não foi sorteado
+                do
+                {
+                    numeroCarta = sorteio.Next(1, 14);  // números de 1 a 13
+                } while (numerosSorteados.Contains(numeroCarta));
+                numerosSorteados.Add(numeroCarta); 
+            
+                carta = naipe + numeroCarta.ToString();
+                
+                // Adiciona o valor de numeroCarta no vetor cartas
+                if (indiceCarta < cartas.Length)
+                {
+                    cartas[indiceCarta] = numeroCarta;
+                    indiceCarta++;
+                }
                 //pic_baralho.Image = Properties.Resources.espada_2;
                 pic_baralho.Image = (System.Drawing.Bitmap)Properties.Resources.ResourceManager.GetObject(carta);
 
@@ -28,7 +43,10 @@ namespace Aula_5
             }
             else
             {
+                carta = naipe + cartas[indiceCarta-1].ToString();
                 pic_baralho.Image = Properties.Resources.capa;
+                pic_cartas.Image = (System.Drawing.Bitmap)Properties.Resources.ResourceManager.GetObject(carta);
+
                 troca = true;
             }
         }

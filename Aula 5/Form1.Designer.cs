9de06a9a0aa2e06dd1fb1ee89ba45d6876881ab9
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2025-04-09 21:32:28 -0300

    Dois jogadores

--- Aula 5/Form1.Designer.cs
+++ Aula 5/Form1.Designer.cs
@@ -30,16 +30,21 @@
         {
             pic_baralho = new PictureBox();
             pic_cartas = new PictureBox();
+            lbl_Contador = new Label();
+            lbl_Contador2 = new Label();
+            pic_cartas2 = new PictureBox();
+            btn_Parar = new Button();
             ((System.ComponentModel.ISupportInitialize)pic_baralho).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pic_cartas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic_cartas2).BeginInit();
             SuspendLayout();
             // 
             // pic_baralho
             // 
             pic_baralho.Image = Properties.Resources.capa;
-            pic_baralho.Location = new Point(21, 12);
+            pic_baralho.Location = new Point(220, 12);
             pic_baralho.Name = "pic_baralho";
-            pic_baralho.Size = new Size(230, 352);
+            pic_baralho.Size = new Size(108, 190);
             pic_baralho.SizeMode = PictureBoxSizeMode.Zoom;
             pic_baralho.TabIndex = 0;
             pic_baralho.TabStop = false;
@@ -47,30 +52,81 @@
             // 
             // pic_cartas
             // 
-            pic_cartas.Location = new Point(289, 12);
+            pic_cartas.Image = Properties.Resources.capa;
+            pic_cartas.Location = new Point(27, 189);
             pic_cartas.Name = "pic_cartas";
-            pic_cartas.Size = new Size(230, 352);
+            pic_cartas.Size = new Size(143, 205);
             pic_cartas.SizeMode = PictureBoxSizeMode.Zoom;
             pic_cartas.TabIndex = 1;
             pic_cartas.TabStop = false;
+            // 
+            // lbl_Contador
+            // 
+            lbl_Contador.AutoSize = true;
+            lbl_Contador.Font = new Font("Segoe UI", 16F);
+            lbl_Contador.Location = new Point(90, 403);
+            lbl_Contador.Name = "lbl_Contador";
+            lbl_Contador.Size = new Size(25, 30);
+            lbl_Contador.TabIndex = 2;
+            lbl_Contador.Text = "0";
+            // 
+            // lbl_Contador2
+            // 
+            lbl_Contador2.AutoSize = true;
+            lbl_Contador2.Font = new Font("Segoe UI", 16F);
+            lbl_Contador2.Location = new Point(437, 403);
+            lbl_Contador2.Name = "lbl_Contador2";
+            lbl_Contador2.Size = new Size(25, 30);
+            lbl_Contador2.TabIndex = 4;
+            lbl_Contador2.Text = "0";
+            // 
+            // pic_cartas2
+            // 
+            pic_cartas2.Image = Properties.Resources.capa;
+            pic_cartas2.Location = new Point(374, 189);
+            pic_cartas2.Name = "pic_cartas2";
+            pic_cartas2.Size = new Size(143, 205);
+            pic_cartas2.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_cartas2.TabIndex = 3;
+            pic_cartas2.TabStop = false;
+            // 
+            // btn_Parar
+            // 
+            btn_Parar.Location = new Point(240, 250);
+            btn_Parar.Name = "btn_Parar";
+            btn_Parar.Size = new Size(75, 23);
+            btn_Parar.TabIndex = 5;
+            btn_Parar.Text = "Parar";
+            btn_Parar.UseVisualStyleBackColor = true;
+            btn_Parar.Click += btn_Parar_Click;
             // 
             // frm_baralho
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(541, 426);
+            ClientSize = new Size(541, 442);
+            Controls.Add(btn_Parar);
+            Controls.Add(lbl_Contador2);
+            Controls.Add(pic_cartas2);
+            Controls.Add(lbl_Contador);
             Controls.Add(pic_cartas);
             Controls.Add(pic_baralho);
             Name = "frm_baralho";
             Text = "Baralho";
             ((System.ComponentModel.ISupportInitialize)pic_baralho).EndInit();
             ((System.ComponentModel.ISupportInitialize)pic_cartas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic_cartas2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pic_baralho;
         private PictureBox pic_cartas;
+        private Label lbl_Contador;
+        private Label lbl_Contador2;
+        private PictureBox pic_cartas2;
+        private Button btn_Parar;
     }
 }
--- Aula 5/Form1.cs
+++ Aula 5/Form1.cs
@@ -5,7 +5,9 @@ namespace Aula_5
         bool troca = true;
         int[] cartas = new int[13];
         int indiceCarta = 0;
+        int pontos = 0;
         List<int> numerosSorteados = new List<int>();
+        int jogador = 1;
 
         public frm_baralho()
         {
@@ -20,16 +22,17 @@ namespace Aula_5
             string naipe = "espada_";
             string carta = "1";
 
-            if (troca) { 
+            if (troca)
+            {
                 // Gera um número único que ainda não foi sorteado
                 do
                 {
                     numeroCarta = sorteio.Next(1, 14);  // números de 1 a 13
                 } while (numerosSorteados.Contains(numeroCarta));
-                numerosSorteados.Add(numeroCarta); 
-            
+                numerosSorteados.Add(numeroCarta);
+
                 carta = naipe + numeroCarta.ToString();
-                
+
                 // Adiciona o valor de numeroCarta no vetor cartas
                 if (indiceCarta < cartas.Length)
                 {
@@ -43,12 +46,78 @@ namespace Aula_5
             }
             else
             {
-                carta = naipe + cartas[indiceCarta-1].ToString();
+                carta = naipe + cartas[indiceCarta - 1].ToString();
                 pic_baralho.Image = Properties.Resources.capa;
-                pic_cartas.Image = (System.Drawing.Bitmap)Properties.Resources.ResourceManager.GetObject(carta);
 
+                if (cartas[indiceCarta - 1] <= 10)
+                    pontos += cartas[indiceCarta - 1];
+                else
+                    pontos += 10;
+
+                if (jogador == 1)
+                {
+                    pic_cartas.Image = (System.Drawing.Bitmap)Properties.Resources.ResourceManager.GetObject(carta);
+                    lbl_Contador.Text = pontos.ToString();
+                }
+                else
+                {
+                    pic_cartas2.Image = (System.Drawing.Bitmap)Properties.Resources.ResourceManager.GetObject(carta);
+                    lbl_Contador2.Text = pontos.ToString();
+                }
+
+               
+
+                // Verifica se o jogador ganhou
+                if (pontos == 21)
+                {
+                    MessageBox.Show("Você ganhou!");
+                    pontos = 0;
+                    lbl_Contador.Text = pontos.ToString();
+                    indiceCarta = 0;
+                    numerosSorteados.Clear();
+                }
+                else if (pontos > 21)
+                {
+                    MessageBox.Show("Você perdeu!");
+                    pontos = 0;
+                    lbl_Contador.Text = pontos.ToString();
+                    indiceCarta = 0;
+                    numerosSorteados.Clear();
+                    //pic_cartas.Image = Properties.Resources.capa;
+                    
+                    //  zerar jogadas dos dois baralhos
+                    if(jogador == 1)
+                    {
+                        jogador = 2;
+                        pic_cartas.Image = Properties.Resources.capa;
+                        pic_cartas2.Image = Properties.Resources.capa;
+                        lbl_Contador.Text = pontos.ToString();
+                    }
+                    else
+                    {
+                        jogador = 1;
+                        pic_cartas.Image = Properties.Resources.capa;
+                        pic_cartas2.Image = Properties.Resources.capa;
+                        lbl_Contador2.Text = pontos.ToString();
+                    }
+                }
+                else
+                {
+                    //MessageBox.Show("Continue jogando!");
+                }
                 troca = true;
             }
+        }
+
+        private void btn_Parar_Click(object sender, EventArgs e)
+        {
+            jogador = 2;
+            MessageBox.Show("Próximo Jogador");
+            pontos = 0;
+            lbl_Contador2.Text = pontos.ToString();
+            indiceCarta = 0;
+            numerosSorteados.Clear();
+            //pic_cartas.Image = Properties.Resources.capa;
         }
     }
 }
